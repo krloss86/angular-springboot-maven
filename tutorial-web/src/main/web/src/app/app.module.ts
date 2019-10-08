ae4c10d4640d471224a53e891da7a5cff329c5be
CX PC <CX PC@DESKTOP-IQFNS1D>
2019-10-08 04:01:46 -0300

    compilacion exitosa aws

--- tutorial-web/src/main/web/src/app/app.module.ts
+++ tutorial-web/src/main/web/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { SaldoComponent } from './saldo/saldo.component';
+import { RecomendacionComponent } from './recomendacion/recomendacion.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    SaldoComponent,
+    RecomendacionComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
