9cfe6758ae4bad630ecaf2798e1f80dce8ea6fb9
carlos horacio lopez <chlopez@boldt.com.ar>
2019-10-11 19:22:00 -0300

    Inclusion de sb2 admin template de boostrap

--- app-web/src/main/web/src/app/home/home.component.ts
+++ app-web/src/main/web/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer2: Renderer2) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+
+  }
+
+  toggle() {
+
+    // this.document.body.classList.add('sidebar-toggled');
+    /*$("body").toggleClass("sidebar-toggled");
+      $(".sidebar").toggleClass("toggled");
+      if ($(".sidebar").hasClass("toggled")) {
+        $('.sidebar .collapse').collapse('hide');
+      };
+    }*/
+  }
 }
--- app-web/src/main/web/src/app/navbar/navbar.component.ts
+++ app-web/src/main/web/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('navBarMenu', {static: false}) navBarMenu: ElementRef;
+
+  navBarExpanded = false;
+
+  constructor(private render: Renderer2) {
+
+  }
 
   ngOnInit() {
   }
 
+  expandMenu() {
+    this.navBarExpanded = !this.navBarExpanded;
+    if (this.navBarExpanded) {
+      this.render.addClass(this.navBarMenu.nativeElement, 'expand');
+    } else {
+      this.render.removeClass(this.navBarMenu.nativeElement, 'expand');
+    }
+    // this.render.addClass(this.navBarMenu.nativeElement, 'animated');
+    // this.render.addClass(this.navBarMenu.nativeElement, 'fadeInLeft');
+  }
 }
