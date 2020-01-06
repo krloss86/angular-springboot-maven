f9aca20bb075f8c9ca2e56b91caa53ffc0a1f35b
clopez <chlopez@boldt.com.ar>
2020-01-06 17:16:34 -0300

    log en interceptores

--- app-web/src/main/web/src/app/login/_helpers/error.interceptor.ts
+++ app-web/src/main/web/src/app/login/_helpers/error.interceptor.ts
@@ -1,15 +1,17 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
 import { AuthenticationService } from '../_services';
+import { Router } from '@angular/router';
+
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+    constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('ErrorInterceptor...');
         return next.handle(request).pipe(catchError(err => {
             if (err.status === 401) {
                 // auto logout if 401 response returned from api
